2b364792ca7de3747a50a52179164a5ce7a0fd58
alibekshak <alibekshakiro1@gmail.com>
2023-11-13 13:03:32 +0600

    first commit

---
+++ Notification/NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationApp.swift
+//  Notification
+//
+//  Created by Apple on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
